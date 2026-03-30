94a0711cd80fcfcc960760393d21628761a9a3b9
Hasan Fatih TURK <hfatih2005@gmail.com>
2026-03-30 19:37:03 +0300

    lists

---
+++ list.py
@@ -0,0 +1,34 @@
+#exam_grades = [80,90,20,100]
+
+#print(type(exam_grades))
+
+"""print(exam_grades)
+exam_grades[1] = 95
+
+print(exam_grades)"""
+
+"""grades = ["kaya" ,12.08 , 0.7, 13]
+print(type(grades[0]))"""
+
+"""cities = ["Istanbul" , "Ankara" , "Bursa"]
+print("first case: ",cities)
+cities[1] = "Izmir"
+print("second case: ",cities)"""
+
+
+"""sehirler = ["Ankara","Tokat"]
+print(sehirler)
+sehirler.append("Bursa")
+print(sehirler)"""
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lists_advanced.py
@@ -0,0 +1,30 @@
+"""otel_zinciri_1 = ["Ankara","Bursa"]
+otel_zinciri_2 = ["Van","Denizli"]
+
+abc_turizm = [otel_zinciri_1,otel_zinciri_2]
+print(abc_turizm[1][1])"""
+
+
+#list = ["Max",60,[70,80,90]]
+#list [2][0] = 90
+#print(list)
+
+#slicing    2.den 5.ye kadar (5 dahil değil)
+meyveler =["elma","armut","muz","çilek","kiraz","şeftali"]
+
+
+"""
+orta_meyveler = meyveler[2:5]
+print(orta_meyveler)
+cift_indeksli = meyveler[::2]
+print(cift_indeksli)"""
+
+#ters yazma
+"""ters_meyveler = meyveler[::-1]
+print(ters_meyveler)"""
+
+"""meyveler_2 = meyveler[::]
+print(meyveler_2)
+meyveler_copy = meyveler.copy()
+print(meyveler_copy)
+"""
