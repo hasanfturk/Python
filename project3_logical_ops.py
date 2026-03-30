56849dac1a6678d78de9fdb5434e62ec8b971289
Hasan Fatih TURK <hfatih2005@gmail.com>
2026-03-30 09:09:28 +0300

    fixed

--- project3_logical_ops.py
+++ project3_logical_ops.py
@@ -5,10 +5,12 @@ if sayi % 2 != 0 and sayi > 20:
 else:
     print(f"{sayi} istenilen kriterleri karşılamıyor.")
 
+
+"""
     sayi = int(input("Bir tamsayı giriniz: "))
 
 
-"""
+
 if sayi % 2 != 0 and sayi > 20:
     print(sayi, " 20'den büyük tek sayıdır.")
 else:
