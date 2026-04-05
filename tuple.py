55d7410df7125b0b20a1393d3f7a682b34f4384d
Hasan Fatih TURK <hfatih2005@gmail.com>
2026-04-05 23:20:06 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#list
+"""
+dersler_liste = ["Fizik","Kimya","Biyoloji"]
+dersler_liste[0]="İngilizce"
+print(dersler_liste)
+"""
+
+dersler_tuple = ("Fizik","Kimya","Matematik")
+#dersler_tuple[0]= "İngilizce"
+print(dersler_tuple)
+
+#tuple örnekleri: mevsimler,aylar...
