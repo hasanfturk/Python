2bc3a3dda8fc40e98da6fde197c83672b65d126f
Hasan Fatih TURK <hfatih2005@gmail.com>
2026-04-08 18:29:43 +0300

    comment update

--- list_and_set_example.py
+++ list_and_set_example.py
@@ -1,4 +1,5 @@
-#verilen listedeki tekrar eden elemanları silip aynı isimde ancak bu sefer içinde tekrar eden eleman olmayan bir liste haline dönüştürünüz.
+#list and set example
+# #verilen listedeki tekrar eden elemanları silip aynı isimde ancak bu sefer içinde tekrar eden eleman olmayan bir liste haline dönüştürünüz.
 
 numbers_list =  [1,2,3,2,4,5,3,6,2,7,5,8]
 numbers_set = set(numbers_list)
