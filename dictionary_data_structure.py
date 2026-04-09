dfd654d57e3015c9cbae3c32d90bf5919ed23fce
Hasan Fatih TURK <hfatih2005@gmail.com>
2026-04-09 21:46:15 +0300

    dictionary

---
+++ dictionary_data_structure.py
@@ -0,0 +1,25 @@
+#dict --> dictionary
+
+ogrenciler_dict = {100:"Ali Kaya",101:"Zeynep Taş"}
+#print(type(ogrenciler_dict))
+#print(ogrenciler_dict)
+#print(ogrenciler_dict[100])
+
+deger = ogrenciler_dict.get(102)
+if deger is None:
+    print("Öğrenci okulda yok.")
+else:
+    print("Öğrenci mevcut.")
+
+liste1 =list(ogrenciler_dict.keys())
+print(liste1)
+
+liste2 =list(ogrenciler_dict.values())
+print(liste2)
+
+#print(type(deger))
+
+"""
+ogrenci_numaralari = [100,101]
+ogrenci_isimleri = ["Ali Kaya","Zeynep Taş"]
+"""
